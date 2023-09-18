5c3ef789904daef8cd33caa25dfe0dd131664ad3
DaaNMSR <142696419+DaaNMSR@users.noreply.github.com>
2023-09-18 23:58:43 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let op;
+function res(){
+		let result;
+		let num1 = Number(document.getElementById('inp1').value);
+		let num2 = Number(document.getElementById('inp2').value);
+	switch(op) {
+	case '+' :
+		result = num1 + num2;
+		break;
+	case '-' :
+		result = num1 - num2;
+		break;
+	case '/' :
+		result = num1 / num2;
+		break;
+	case '*' :
+		result = num1 * num2;
+		break;
+	}
+        document.getElementById('result').innerHTML = result;
+	}
